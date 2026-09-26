7792123ea4f17cf930a2333f8dbfc732325951ff
shreyash-dheple <shreyash.s.dheple>
2026-09-26 19:53:02 +0530

    create Room class

--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,36 @@
 class Player:
    
-    def __init__(self, name, player_id, ):
+    def __init__(self, name, player_id,):
         self.name = name
         self.id = player_id
+        self.is_alive = True
+
 
     def eliminate(self):
-        return False
+        self.is_alive = False
+
+
+class Room:
+
+    def __init__(self, room_code):
+        self.room_code = room_code == "XY"
+        self.players = {}
+        self.starting_letter = "SH"
+
+    def add_player(self, player):
+        self.players[player.player_id] = {
+            
+        }
+       
+room = Room("XY")
+Player = room.add_player(77)
+print(Room.players)
+       
+    
+   
+        
+        
+
 
 
 
